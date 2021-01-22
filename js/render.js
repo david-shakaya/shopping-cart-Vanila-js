b1d7b5bca5463d8ba341301015b3918976f5160b
david-shakaya <devidoz666@gmail.com>
2021-01-22 21:13:28 +0200

    ищет ошибку

--- js/render.js
+++ js/render.js
@@ -4,7 +4,7 @@ import refs from './refs.js'
 import cart from './shopping-cart.js';
 
 const getProducts= async () => {
-    const response = await fetch('../products.json');
+    const response = await fetch('./products.json');
     const products = response.json();
   return products;
 };
